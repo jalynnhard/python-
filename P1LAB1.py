4266f1d97ca3a6b12c9b676b06ca03491731db86
jalynnhard <160151119+jalynnhard@users.noreply.github.com>
2024-02-15 10:25:39 -0500

    first code for python

---
+++ P1LAB1.py
@@ -0,0 +1,11 @@
+#Jalynn Hardin
+#2/13/24
+#P1LAB1
+#using input and output
+
+#Create a variable and get input from users
+name=input("Enter your first and last name: ")
+
+#output a message using the variable
+print("Hey", name, "\nWelcome to zybooks!")
+#print("welcome toi zybooks!")
