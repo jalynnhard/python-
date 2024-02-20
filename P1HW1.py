d60ccfffd196d0a97b0ff9bfe0abc466328c471c
jalynnhard <160151119+jalynnhard@users.noreply.github.com>
2024-02-20 17:21:13 +0000

    update

---
+++ P1HW1.py
@@ -0,0 +1,17 @@
+#name jalynn Hardin 
+#Date:2/20/24
+#P1HW1
+
+num1 = int(input("Enter integer"))
+
+print("you entered",num1)
+
+print(num1,"squared is",num1**2)
+
+print(num1,"cubed is",num1**3)
+
+num2 = int(input("Enter another integer"))
+
+print(num1, "+", num2,"is",num1+num2 )
+
+print(num1, "*" ,num2,"is",num1*num2)
