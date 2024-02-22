1e0e3fe66372de13df5c51e6c1b590ce6f940edd
jalynnhard <160151119+jalynnhard@users.noreply.github.com>
2024-02-22 16:15:40 +0000

    update

---
+++ P1HW2.py
@@ -0,0 +1,10 @@
+#Jalynn Hardin 
+#Date:2/22/2024
+#P1HW2
+
+#This program calculates and displays travel expenses
+budget = float(input("Enter your budget"))
+
+#Enter your destination
+destination = input("Enter your travel destination")
+
