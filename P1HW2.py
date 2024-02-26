ed5667cee777188c1715a8da4db9c1415176533e
jalynnhard <160151119+jalynnhard@users.noreply.github.com>
2024-02-26 01:21:29 +0000

    updated

--- P1HW2.py
+++ P1HW2.py
@@ -1,10 +1,22 @@
 #Jalynn Hardin 
 #Date:2/22/2024
 #P1HW2
-
 #This program calculates and displays travel expenses
-budget = float(input("Enter your budget"))
+
+
+budget = float(input("Enter your budget: "))
 
 #Enter your destination
-destination = input("Enter your travel destination")
+destination = input("Enter your travel destination: ")
 
+#gas expenses
+gas_expenses = float(input("enter the amount you will spend on gas: "))
+
+#How much will you spend on accomindations 
+accomindation_expenses = float(input("Enter how much you will spenmmd on accomindations: "))
+
+#How much will you spend on food
+food_expenses = float(input("Enter how much you spent on food: "))
+
+#Add all expenses 
+totall_expenses = float(input("Enter how much "))
