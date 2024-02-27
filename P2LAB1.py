a116a87a873abd90ce6f710cb78e4457a96294b1
jalynnhard <160151119+jalynnhard@users.noreply.github.com>
2024-02-27 16:19:56 +0000

    update

--- P2LAB1.py
+++ P2LAB1.py
@@ -1,11 +1,22 @@
-#F-strings 
+#jALYNN hARDIN
+#dATE:2/27/24
+#p2lab1 = math expressions and f-strings
 
-pay = 600
-heat = 50.25
-rent = 300.6
+mpg = float(input("Enter the cars mpg: "))
+cost_gas = float(input("how much is a galoinr of gas: "))
 
-print(f"weekly pay: {pay:2.f}")
-print(f"heat: {heat:2.f}")
-print(f"rent: {rent:2.f}")
+#Calculate cost of driving 20 miles
+miles_20 = (20/mpg) * cost_gas
 
-print(f"combined bill: ${(heat + rent:.2f}")
+#Calculate cost of driving 75 miles
+miles_75 = (75/mpg) * cost_gas
+
+#Calculate cost of driving 500 miles
+miles_500 = (500/mpg) * cost_gas
+
+#output info to user 
+print(f"cost to drive 20 miles is ${miles_20:.2f}")
+print(f"cost to drive 75 miles is ${miles_75:.2f}")
+print(f"cost to drive 500 miles is ${miles_500:.2f}")
+
+
