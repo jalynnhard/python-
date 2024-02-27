f7deb004a736c5c539b3a8cea1be52046ff92695
jalynnhard <160151119+jalynnhard@users.noreply.github.com>
2024-02-27 16:43:01 +0000

    update

---
+++ P2LAB2.py
@@ -0,0 +1,23 @@
+#Jalynn Hardin 
+#Date:2/27/24
+#P2LAB2 - math expressions and f-strings 
+
+#Get input from user
+num1 = float(input("Enter a float: "))
+num2 = float(input("Enter a float: "))
+num3 = float(input("Enter a float: "))
+num4 = float(input("Enter a float: "))
+
+#Calculate product of variable
+product = num1 * num2 * num3 * num4
+
+#Calculate the average
+average = (num1 + num2 + num3 + num4) / 4
+
+#Display info to user
+print(f"The product is: {product:.0f}")
+print(f"The average is: {product:.0f}")
+
+#Display info to user
+print(f"The product is: {product:.3f}")
+print(f"The average is: {product:.3f}")
