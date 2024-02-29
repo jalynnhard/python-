6104fea90c71a07d096c0ab117c8e29de04c9976
jalynnhard <160151119+jalynnhard@users.noreply.github.com>
2024-02-29 15:56:21 +0000

    update

---
+++ Dictionary(practice).py
@@ -0,0 +1,19 @@
+#Dictionary - each item has a key and value
+
+cars = {"Yesla":"model s", "toyota":"prius", "chevy":"corola"}
+
+#Get item from dictionary
+print(cars["toyota"])
+
+#see all keys in dictionary
+print(list(cars.keys()))
+
+print(list(cars.values()))
+
+#Add to dictionary
+cars["ford"] = "f-150"
+
+#Print the dictionary 
+print(cars)
+
+#Edit item in list
