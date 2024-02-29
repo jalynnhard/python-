6e449c5e62849c80d1aee8a9662c72c4fc1d6db0
jalynnhard <160151119+jalynnhard@users.noreply.github.com>
2024-02-29 15:43:01 +0000

    update

---
+++ P2HW1(practice).py
@@ -0,0 +1,29 @@
+#list 
+
+#Empy list 
+pet_list = {}
+
+#list with items in it
+pet_list = {"Betty", "William", "Yoshi" , "Moon pie" , 2.5}
+
+#Get item from list by the index 
+print(f"{pet_list{3}}")
+
+weight = pet_list[4]
+
+print(weight + 10)
+
+#Adding item list 
+pet_list.append("pebbles")
+
+#print the list
+print(pet_list)
+
+#Remove item from pet list
+pet_list.pop[4]
+
+#print the list 
+print(pet_list)
+
+
+
