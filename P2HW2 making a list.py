19e666d3f1156e805c55cf602b33d550f9b4487e
jalynnhard <160151119+jalynnhard@users.noreply.github.com>
2024-03-05 15:38:25 +0000

    update

--- P2HW2 making a list.py
+++ P2HW2 making a list.py
@@ -7,3 +7,30 @@ nums.append(5.785)
 nums.append(63.0)
 
 print(nums)
+
+num1 = float(input("Enter a float value: "))
+
+#Add variable to list
+nums.append(num1)
+
+nums.append(float(input("Enter a float value: ")))
+
+
+#gET THE SUM OF ALL VALUES IN THE LIST
+list_sum = sum(nums)
+print(f"The sum of the list is : {list_sum}")
+
+print(sum([1,1,1,1,1,1,1,]))
+
+#Get the minimum value from the list
+list_min = min(nums)
+print(list_min)
+print(f"The mallest value of the list is : {list_min}")
+
+#Get maximum value from the list
+list_max = max(nums)
+print(f"The largest value of the list is : {list_max}")
+
+#Get the length of the list
+list_length = len(nums)
+print(f"The list has {list_length} items")
