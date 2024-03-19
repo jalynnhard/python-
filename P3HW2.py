3ed659378c431a60282e1c8eb7c5b5a47ead3af9
jalynnhard <160151119+jalynnhard@users.noreply.github.com>
2024-03-19 14:43:11 +0000

    update

---
+++ P3HW2.py
@@ -0,0 +1,14 @@
+#Jalynn Hardin
+#3/19/24
+
+
+#Enter name
+name=input("Enter employee's name: ")
+
+#Enter numbers of hours worked
+hours=input("Enter numbers of hours worked: ")
+
+#EEnter pay rate
+Pay_rate=float(input("Enter employee's pay rate: "))
+print("------------------------------------------")
+
