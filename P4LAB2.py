6940b60ecbd330b2db87938885e953ab64e34cda
jalynnhard <160151119+jalynnhard@users.noreply.github.com>
2024-03-26 15:47:19 +0000

    update

---
+++ P4LAB2.py
@@ -0,0 +1,16 @@
+#Jalynn Hardin
+#3/26/24
+#P4LAB2
+#Using the range function
+
+mum1 = int(input("Enter a number: "))
+mum2 = int(input("Enter a number: "))
+
+#While loop - as long as num1 is smaller than num2,increase by 5 each time
+
+while num1 > num2: #Bad  
+    print("first number must be smaller ")
+    num1 = int(input("Enter a number: "))
+    num2 = int(input("Enter a number: "))
+#Code will heppen Only when loop breaks
+print("YOU DID IT RIGHT!")
