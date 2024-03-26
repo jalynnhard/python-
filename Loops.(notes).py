85c27ab0611db8713a3abede35aeb7595e3ed682
jalynnhard <160151119+jalynnhard@users.noreply.github.com>
2024-03-26 14:57:26 +0000

    update

---
+++ Loops.(notes).py
@@ -0,0 +1,12 @@
+#Loops
+
+#For loop using range function
+
+counter = 0
+#Here range
+for item in range(4):
+    counter += 2
+    print("counter")
+
+#Outside the loop 
+print("The final value is",counter)
