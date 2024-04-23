37374a7d906c309eb7d2737915d993af9db8472b
jalynnhard <160151119+jalynnhard@users.noreply.github.com>
2024-04-23 14:39:57 +0000

    update

---
+++ menu employee.py
@@ -0,0 +1,11 @@
+#Using a menu-driven program
+
+print("choose an option:Multiply, divide, Exit")
+
+option = input("Enter an option: ")
+while option != "exit":
+    if option == "multiply":
+        print("Here would be your function for multiply")
+    if option == "Divide"
+        print("Here would be your function for division")
+        
